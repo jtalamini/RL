gradient ascent on bandit problem and fixes..

bandit_incremental_value.py
@@ -54,9 +54,8 @@ while run < RUNS:
         N[arm] += 1
         R.append(r)
         pulled_arms.append(arm)
-        if episode % 10 == 0:
-            # update action value function
-            Q[arm] += estimate(arm)
+        # update action value function
+        Q[arm] += estimate(arm)
     score.append(np.sum(R)/EPISODES)
     if np.argmax(arms_prob) == np.argmax(Q): accuracy += 1.0
 

bandit_sample_average.py
@@ -52,12 +52,11 @@ while run < RUNS:
         r = pull_arm(arm)
         R.append(r)
         pulled_arms.append(arm)
-        if episode % 10 == 0:
-            # update action value function
-            R_array = np.array(R)
-            A_array = np.array(pulled_arms)
-            for i in range(k):
-                Q[i] = estimate(i)
+        # update action value function
+        R_array = np.array(R)
+        A_array = np.array(pulled_arms)
+        for i in range(k):
+            Q[i] = estimate(i)
     score.append(np.sum(R)/EPISODES)
     if np.argmax(arms_prob) == np.argmax(Q): accuracy += 1.0
 

bandit_stochastic_gradient.py
@@ -44,13 +44,12 @@ while run < RUNS:
         r = pull_arm(arm)
         R.append(r)
         pulled_arms.append(arm)
-        if episode % 10 == 0:
-            '''
-            update preference function H
-            stochastic gradient ascent:
-            each action preference H(a) is incremented proportionally to its effect on performances dE(R)/dH
-            '''
-            H += alpha * (R[-1] - np.mean(R)) * (y - policy)
+        '''
+        update preference function H
+        stochastic gradient ascent:
+        each action preference H(a) is incremented proportionally to its effect on performances dE(R)/dH
+        '''
+        H += alpha * (R[-1] - np.mean(R)) * (y - policy)
     score.append(np.sum(R)/EPISODES)
     if np.argmax(arms_prob) == np.argmax(H): accuracy += 1.0
 